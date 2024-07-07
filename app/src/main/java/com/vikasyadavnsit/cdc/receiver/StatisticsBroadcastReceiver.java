e10fa8d8b201ab1149ec4bbd60ee2c331c8c4c46
Vikas Yadav <vickytauruss@gmail.com>
2024-07-07 12:53:53 +0530

    feat: implement statistics broadcast for power button events

---
+++ app/src/main/java/com/vikasyadavnsit/cdc/receiver/StatisticsBroadcastReceiver.java
@@ -0,0 +1,47 @@
+package com.vikasyadavnsit.cdc.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.vikasyadavnsit.cdc.constants.AppConstants;
+import com.vikasyadavnsit.cdc.services.ResetService;
+
+public class StatisticsBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction() != null) {
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_ON:
+                    context.startService(new Intent(context, ResetService.class).setAction(Intent.ACTION_SCREEN_ON));
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    context.startService(new Intent(context, ResetService.class).setAction(Intent.ACTION_SCREEN_OFF));
+                    break;
+                case Intent.ACTION_SHUTDOWN:
+                case Intent.ACTION_REBOOT:
+                case Intent.ACTION_BOOT_COMPLETED:
+                case Intent.ACTION_AIRPLANE_MODE_CHANGED:
+                case Intent.ACTION_POWER_CONNECTED:
+                case Intent.ACTION_POWER_DISCONNECTED:
+                case Intent.ACTION_PACKAGE_ADDED:
+                case Intent.ACTION_USER_PRESENT:
+                    Log.d("ResetBroadcastReceiver", "onReceive:" + AppConstants.ACTION_APPLICATION_RESET_USAGE);
+                    // Handle application installed
+                   // String packageName = intent.getData().getSchemeSpecificPart();
+                    //Log.d("TAG", "Installed: " + packageName);
+                    break;
+                case Intent.ACTION_PACKAGE_REMOVED:
+                    // Handle application uninstalled
+                    String packageNameRemoved = intent.getData().getSchemeSpecificPart();
+                    Log.d("TAG", "Uninstalled: " + packageNameRemoved);
+                    break;
+
+
+            }
+        }
+
+    }
+}
