8d47b465495ad17896c4510b4e85402cb7413e12
Vikas Yadav <vickytauruss@gmail.com>
2024-07-21 23:12:21 +0530

    Integrated extra lines in notification

--- app/src/main/java/com/vikasyadavnsit/cdc/data/NotificationData.java
+++ app/src/main/java/com/vikasyadavnsit/cdc/data/NotificationData.java
@@ -1,5 +1,8 @@
 package com.vikasyadavnsit.cdc.data;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -16,6 +19,7 @@ public class NotificationData {
     private String summaryText;
     private String infoText;
     private String conversationTitle;
+    private String[] extras;
     private String packageName;
     private String timestamp;
 
